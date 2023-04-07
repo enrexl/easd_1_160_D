1dc0875c77253e7a3d6f6a95f751ff440625b090
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-04-07 09:06:54 +0700

    Add project files.

---
+++ Exercise1/Exercise1.cpp
@@ -0,0 +1,19 @@
+//1. Jelaskan, mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah.
+// 
+//2. Dalam algorithma, di klassifikasikan menjadi 2 data struktur.Sebutkan 2 data struktur tersebut.
+// 
+//3. Sebutkan faktor - faktor yang mempengaruhi mempengaruhi efisiensi suatu program algorithma!
+// 
+//4. Dari 6 algorithma yang sudah dipelajari.Algortihma manakah yang paling efisien untuk mengurutkan data yang ukurannya kecil.Jelaskan kenapa anda memilih itu.
+// 
+//5. Sebutkan dari algorithma yang telah dipelajari mana yang termasuk kedalam Quadratic dan mana yang termasuk kedalam Loglinear.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+Jelaskan, mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah
