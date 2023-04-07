20d05880a594176d5cfe2b3e01ec9539984fa3ff
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-04-07 09:20:14 +0700

    Menjawab pertnyaan dari pdf

--- Exercise1/Exercise1.cpp
+++ Exercise1/Exercise1.cpp
@@ -1,13 +1,16 @@
 //1. Jelaskan, mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah.
-// 
+//  - Algoritma memiliki cara penyelesaian sebuah masalah secara struktural, efisien, dan mendetail.
 //2. Dalam algorithma, di klassifikasikan menjadi 2 data struktur.Sebutkan 2 data struktur tersebut.
-// 
+//  - Linear dan Binary
 //3. Sebutkan faktor - faktor yang mempengaruhi mempengaruhi efisiensi suatu program algorithma!
-// 
+//  - Banyaknya data
+//  - Banyaknya fungsi yang dipanggil
+//  - Banyaknya variable
 //4. Dari 6 algorithma yang sudah dipelajari.Algortihma manakah yang paling efisien untuk mengurutkan data yang ukurannya kecil.Jelaskan kenapa anda memilih itu.
-// 
+//  - Jika data yang diurutkan ukurannya kecil, maka InsertionSort merupakan cara pengurutan data yang paling efisien karena bekerja dengan cara membandingkan data yang ada di sebelahnya.
 //5. Sebutkan dari algorithma yang telah dipelajari mana yang termasuk kedalam Quadratic dan mana yang termasuk kedalam Loglinear.
-//
+//  - Quadratic : BubbleSort, InsertionSort, SelectionSort.
+//  - Loglinear : QuickSort, MergeSort, ShellSort.
 
 #include <iostream>
 using namespace std; 
